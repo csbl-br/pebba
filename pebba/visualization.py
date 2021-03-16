af3da708e7fca6e26655866b217fc8928aa12a8e
alan-barzilay <alan.barzilay@gmail.com>
2021-03-15 22:46:01 -0300

    New colors and layout for the visualizations
    Plots now share axis and changes in one plot are reflected in the others, such as selecting a subspace of the plot
    New colormap for the plots, one red and one blue version
    Added a black frame to the plots
    Fixed legends and barcolor superimposing each other
    Converted line plot into a barplot
    Removed legends for the bar plots
    
    Signed-off-by: Alan Barzilay and Rafael Badain

--- pebba/visualization.py
+++ pebba/visualization.py
@@ -13,13 +13,64 @@ def create_interactive_plot(
     line_plot = plot_number_of_enriched_pathways(statistics_for_plot, direction)
 
     data = [heatmap, barplot, line_plot]
+
     layout = go.Layout(
-        xaxis=dict(domain=[0, 0.80], showticklabels=False),
-        xaxis2=dict(domain=[0, 0.80], showticklabels=False),
-        xaxis3=dict(domain=[0.85, 1], anchor="y3"),
-        yaxis=dict(domain=[0.30, 1]),
-        yaxis2=dict(domain=[0, 0.25], autorange="reversed", anchor="x2"),
-        yaxis3=dict(domain=[0.30, 1], anchor="x3"),
+        showlegend=False,
+        # paper_bgcolor="rgba(0,0,0,0)",  # background color for the paper of the plot
+        plot_bgcolor="rgb(255,255,255)",  # background color for the plot
+        xaxis=dict(
+            domain=[0, 0.80],
+            showticklabels=False,
+            mirror=True,
+            showline=True,
+            linewidth=1,
+            linecolor="rgb(33, 27, 22)",
+            matches="x2",
+        ),
+        xaxis2=dict(
+            domain=[0, 0.80],
+            showticklabels=False,
+            mirror=True,
+            showline=True,
+            linewidth=1,
+            linecolor="rgb(33, 27, 22)",
+        ),
+        xaxis3=dict(
+            domain=[0.85, 1],
+            anchor="y3",
+            mirror=True,
+            showline=True,
+            linewidth=1,
+            linecolor="rgb(33, 27, 22)",
+            gridcolor="rgb(200, 200, 200)",
+            autorange="reversed",
+        ),
+        yaxis=dict(
+            domain=[0.30, 1],
+            mirror=True,
+            showline=True,
+            linewidth=1,
+            linecolor="rgb(33, 27, 22)",
+            matches="y3",
+        ),
+        yaxis2=dict(
+            domain=[0, 0.25],
+            autorange="reversed",
+            anchor="x2",
+            mirror=True,
+            showline=True,
+            linewidth=1,
+            linecolor="rgb(33, 27, 22)",
+            gridcolor="rgb(200, 200, 200)",
+        ),
+        yaxis3=dict(
+            domain=[0.30, 1],
+            anchor="x3",
+            mirror=True,
+            showline=True,
+            linewidth=1,
+            linecolor="rgb(33, 27, 22)",
+        ),
     )
     figure = go.Figure(data=data, layout=layout)
 
@@ -34,7 +85,22 @@ def create_heatmap(df):
     pathways = df.index.tolist()
     values = [df[column].tolist() for column in df]
 
-    trace = go.Heatmap(z=values, y=NGs, x=pathways)
+    trace = go.Heatmap(
+        z=values,
+        y=NGs,
+        x=pathways,
+        colorscale=
+        # ["rgb(255,255,255)", "rgb(229, 45, 39)", "rgb(179, 18, 23)"], # red
+        ["rgb(255,255,255)", "rgb(47, 187, 237)", "rgb(41, 128, 185)"],  # blue
+        colorbar={
+            "x": 0.8,
+            "len": 0.7,
+            "y": 1,
+            "yanchor": "top",
+            # "ticklabelposition": "inside top",
+            # "tickfont": {"color": "rgb(0,0,0)"},
+        },
+    )
     return trace
 
 
@@ -45,18 +111,26 @@ def create_barplot(dict_genes_por_via, df):
         orientation="v",
         xaxis="x2",
         yaxis="y2",
+        marker={
+            "color":
+            # "rgb(135, 57, 57)", #red
+            "rgb(126, 139, 158)",  # blue
+        },
     )
     return barplot
 
 
 def plot_number_of_enriched_pathways(statistics_for_plot, direction):
-    plot = go.Scatter(
+    barplot = go.Bar(
         x=statistics_for_plot["times_significant_" + direction].tolist(),
         y=statistics_for_plot.index.tolist(),
         orientation="h",
-        mode="lines+markers",
-        name="lines+markers",
         xaxis="x3",
         yaxis="y3",
+        marker={
+            "color":
+            # "rgb(135, 57, 57)",
+            "rgb(126, 139, 158)",
+        },
     )
-    return plot
+    return barplot
